ebe86558d721ab3aad4f702b5755dc7d92e5597e
coding <1225335417@qq.com>
2024-11-18 01:46:49 +0800

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,161 +1,97 @@
-import Image from 'next/image';
-import localFont from 'next/font/local';
-import { useTranslation } from 'react-i18next';
+import { useEffect, useRef } from 'react';
+import { setShowVideo } from '@/redux/modules/common';
 import { useDispatch } from 'react-redux';
 import { showNotification } from '@/redux/modules/notificationSlice';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
-import { useEffect } from 'react';
-export default function Home() {
+const Index = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const dispatch = useDispatch();
-  const { t } = useTranslation();
+  // 监听视频是否进入视口
   useEffect(() => {
-    const fetchHello = async () => {
-      const response = await fetch(
-        '/api/user',
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // 对每个观察到的条目进行处理
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // 视频进入视口，开始播放
+            videoRef.current?.play();
+            console.log('play');
+            dispatch(setShowVideo(true));
+          } else {
+            // 视频离开视口，暂停播放
+            videoRef.current?.pause();
+            console.log('pause');
+          }
+          dispatch(setShowVideo(entry.isIntersecting));
+        });
+      },
+      {
+        threshold: 0.1, // 当视频有50%进入视口时触发
+      }
+    );
 
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            address: '0xE3a463d743F762D538031BAD3f1E748BB41f96ec',
-          }),
-        }
-      );
-      const data = await response.json();
-      console.log(data.message); // 输出: "Hello, TypeScript!"
+    // 观察视频元素
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    // 清理 observer
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
     };
-    fetchHello();
   }, []);
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      {/* <Button type="primary" onClick={() => {}}>
-        显示通知
-      </Button> */}
-      <div>
-        <h1>{t('greeting')}</h1>
-        <p>{t('welcomeMessage')}</p>
-      </div>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          onClick={() => {
-            // notification.success({
-            //   message: '成功通知',
-            //   description: '这是一个成功的消息通知。',
-            //   placement: 'topRight',
-            // });
-            dispatch(
-              showNotification({
-                type: 'success',
-                message: 'Success!',
-                description: 'This is a success message.',
-              })
-            );
-          }}
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const clickOpensea = () => {
+    dispatch(
+      showNotification({
+        type: 'error',
+        message: 'Success!',
+        description: '功能未开发',
+      })
+    );
+  };
+
+  return (
+    <div>
+      <div className="relative">
+        {/* <Button type="primary" onClick={clickOpensea}>
+          显示通知
+        </Button> */}
+        <video
+          ref={videoRef}
+          loop
+          autoPlay
+          muted
+          src="https://phanta-bear-nft.matrixlabs.org/website/videos/homepage_video.mp4"
+        ></video>
+        <div
+          onClick={clickOpensea}
+          className=" font-bold bottom-[20%] absolute left-1/2 -translate-x-[calc(50%-10px)]  bg-btnbg px-16 py-4 box-border cursor-pointer"
+        >
+          <span className="text-3xl">去OPENSEA查看</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          onClick={clickOpensea}
+          className="font-bold bottom-[calc(20%+10px)] absolute left-1/2 transform  -translate-x-1/2 bg-[#ffffff]  px-16 py-4 border-[1px] border-solid border-btnbg cursor-pointer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <span className="text-3xl">去OPENSEA查看</span>
+        </div>
+      </div>
+      <div className="px-28 w-full overflow-hidden">
+        <div className="flex py-10">
+          <div className="flex-1 text-[100px]  xl:text-[144px] font-bold font-[Amatic SC]">
+            INTRODUCTION
+          </div>
+          <img
+            className="hidden"
+            src="https://ezek.io/static/headerPicture-0a8747b91a51ad35b937d41b5fc4ab64.png"
+            alt="Header"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Index;

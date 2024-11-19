f484abaadbf78611a14e4b312623b8a72365f752
coding <1225335417@qq.com>
2024-11-19 16:55:27 +0800

    x

--- nextjs/src/config/wagmi.ts
+++ nextjs/src/config/wagmi.ts
@@ -1,15 +1,12 @@
-import { mainnet, sepolia,arbitrum,arbitrumSepolia } from 'wagmi/chains'
-import {
-  getDefaultConfig,
-} from '@rainbow-me/rainbowkit';
+import { mainnet, sepolia, arbitrum, arbitrumSepolia } from 'wagmi/chains';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-const WALLET_CONNECT = process.env.NEXT_PUBLIC_WALLET_CONNECT || "";
-
+const WALLET_CONNECT = process.env.NEXT_PUBLIC_WALLET_CONNECT || '';
 
 const config = getDefaultConfig({
   appName: 'Base_web3',
-  projectId: WALLET_CONNECT,
-  chains: [mainnet, sepolia,arbitrum,arbitrumSepolia ],
+  projectId: '2d3a12345c1132ba152fd8f51764915a',
+  chains: [mainnet, sepolia, arbitrum, arbitrumSepolia],
   ssr: true,
 });
 
